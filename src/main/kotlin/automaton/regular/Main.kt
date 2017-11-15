6291c1fab274145510d7947de1fa70e947fa11fd
Alexandru Stoica <stoica.alexandru2000@gmail.com>
2017-11-15 10:53:16 +0200

    Final

--- src/main/kotlin/automaton/regular/Main.kt
+++ src/main/kotlin/automaton/regular/Main.kt
@@ -1,5 +1,7 @@
 package automaton.regular
 
+import automaton.regular.displayer.FiniteAutomatonDisplayer
+import automaton.regular.displayer.GrammarDisplayer
 import automaton.regular.reader.FiniteAutomatonReader
 import automaton.regular.reader.GrammarReader
 import java.io.File
@@ -12,8 +14,8 @@ import java.io.File
 fun main(args: Array<String>) {
     val grammarsFromFile =
             GrammarReader().readMultipleFromFile(File("testing_grammars.json"))
-    grammarsFromFile.forEach { println(it) }
     val automatonsFromFile =
             FiniteAutomatonReader().readMultipleFromFile(File("testing_finite_automaton.json"))
-    automatonsFromFile.forEach { println(it) }
+    GrammarDisplayer(grammarsFromFile[0]).start()
+    FiniteAutomatonDisplayer(automatonsFromFile[0]).start()
 }
